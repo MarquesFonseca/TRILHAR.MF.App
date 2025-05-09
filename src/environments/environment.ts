effd6c282efbedfc470a92470bf1b6714c453bb2
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2025-05-09 13:45:35 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    API_TRILHAR: `https://iques.com.br/api`,
+    API_TRILHAR: `https://iques.com.br.15-235-55-95.hostsrv.org/api`,
+    //API_TRILHAR: `https://iques.com.br/api`,
     //API_TRILHAR: `https://trilhar-api.azurewebsites.net/api`,
     //API_TRILHAR: "https://localhost:7247/api",
   };
